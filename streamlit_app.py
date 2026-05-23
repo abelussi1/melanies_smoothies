65f49c6b2bc77e3410fe0ae3c8e60f5b43999ccd
alittlerain🌧️ <142629758+abelussi1@users.noreply.github.com>
2026-05-23 13:42:31 -0400

    Update streamlit_app.py
    Add SEARCH_ON col to select statement

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,7 +23,7 @@ st.write(
 name_on_order = st.text_input('Name on smoothie')
 st.write('The name on your Smoothie will be ', name_on_order)
 
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
 # st.dataframe(data=my_dataframe, use_container_width=True)
 # st.stop()
 
