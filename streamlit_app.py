32ece1eb3b86fc856ef50e752e47167fa526a9b6
alittlerain🌧️ <142629758+abelussi1@users.noreply.github.com>
2026-05-23 12:50:56 -0400

    Update streamlit_app.py
    fix dataframe, object of choice

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,9 +32,6 @@ ingredients_list = st.multiselect(
     , max_selections=5
 )
 
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
-
 # display the ingredients_list IF the ingredients_list exists
 if ingredients_list: 
 
@@ -42,6 +39,8 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
     #st.write(ingredients_string)
 
